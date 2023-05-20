baf24a253f2fb964ca8d72d1dcc7a2d6b6541b85
VadimTotok <112573798+VadimTotok@users.noreply.github.com>
2023-05-20 22:06:03 +0300

    Update Roll.java

--- src/main/java/spigot/dicegame/commands/Roll.java
+++ src/main/java/spigot/dicegame/commands/Roll.java
@@ -1,9 +1,9 @@
 package spigot.dicegame.commands;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.util.Random;
 
@@ -11,11 +11,17 @@ public class Roll implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        Random random = new Random();
-
-        for (int i = 0; i < 1; i++) {
-            Bukkit.broadcastMessage( sender.getName() + "* " + "rolls the dice " + (random.nextInt(6) + 1));
+        if (!(sender instanceof Player)) {
+            return true;
         }
+
+        Random random = new Random();
+        Player player = (Player) sender;
+
+        int numDice = 1;
+
+        int rollResult = random.nextInt(6) + 1;
+        player.sendMessage(String.format("%s rolled %d", player.getName(), rollResult));
 
         return true;
     }
